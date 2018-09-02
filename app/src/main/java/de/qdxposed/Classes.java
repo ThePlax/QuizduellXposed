625460c50ecc43ebf61155737094982de7d83d22
ThePlax <hecke.jonas@web.de>
2018-09-02 18:10:11 +0200

    Update Classes.java (only right answer)

--- app/src/main/java/de/qdxposed/Classes.java
+++ app/src/main/java/de/qdxposed/Classes.java
@@ -6,27 +6,9 @@ package de.qdxposed;
 
 public class Classes {
 
-    public static Class AlternativeButton;
-    public static Class AlternativeButtons;
-    public static Class QuestionInterface;
-    public static Class AlternativeInterface;
-    public static Class QkSettingsHelper;
-
-
-    public static class old_names {
-        public static String AlternativeButton = "se.feomedia.quizkampen.act.game.AlternativeButton";
-        public static String AlternativeButtons = "se.feomedia.quizkampen.act.game.AlternativeButtons";
-        public static String QuestionInterface = "se.feomedia.quizkampen.modelinterfaces.Question";
-        public static String AlternativeInterface = "se.feomedia.quizkampen.modelinterfaces.Alternative";
-        public static String QkSettingsHelper = "se.feomedia.quizkampen.helpers.QkSettingsHelper";
-
-    }
+    public static Class BaseQuestion;
 
     public static class new_names {
-        public static String AlternativeButton = "se.feomedia.quizkampen.view.activities.game.AlternativeButton";
-        public static String AlternativeButtons = "se.feomedia.quizkampen.view.activities.game.AlternativeButtons";
-        public static String QuestionInterface = "se.feomedia.quizkampen.data.db.base.Question";
-        public static String AlternativeInterface = "se.feomedia.quizkampen.data.db.base.Alternative";
-        public static String QkSettingsHelper = "se.feomedia.quizkampen.data.helpers.QkSettingsHelper";
+        public static String BaseQuestion = "se.feomedia.quizkampen.domain.BaseQuestion";
     }
 }
